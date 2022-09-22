c47293f64271f9f99af3654f1be2f604937007eb
AndreyDMA <andrii.dmytriev@advancedcommunities.com>
2022-09-22 17:29:58 +0300

    compose html table

--- sf-report-to-pdf/force-app/main/default/lwc/converter/converter.js
+++ sf-report-to-pdf/force-app/main/default/lwc/converter/converter.js
@@ -1,5 +1,7 @@
 import { LightningElement, api } from 'lwc';
+import { composeHeaderHTML, composeTableHTML, composeListHTML } from './converterHelper.js';
 import retrieveReport from '@salesforce/apex/ReportConverterController.retrieveReport';
+import SystemModstamp from '@salesforce/schema/Account.SystemModstamp';
 
 export default class Converter extends LightningElement {
     @api reportId;
@@ -7,15 +9,40 @@ export default class Converter extends LightningElement {
     @api target;
     reportData;
     reportDescr;
+    reportHTML='';
 
     doConvert() {
+        this.loadData();
+        this.composeData();
+    }
+
+    loadData() {
         retrieveReport({reportId : this.reportId})
         .then(result => {
             this.reportData = JSON.parse(result.data);
             this.reportDescr = JSON.parse(result.description);
-            console.log(this.reportData);
-            console.log(this.reportDescr);
         })
         .catch(error => {console.log(error.message)})
     }
+
+    composeData() {
+        if (this.target === "HTML") {
+            this.composeHTML();
+        } else if (this.target === "PDF") {
+            this.composePDF();
+        }
+    }
+
+    composeHTML() {
+        this.reportHTML += composeHeaderHTML(this.reportDescr);
+        
+        if (this.view === 'List view') {
+            this.reportHTML += composeListHTML(this.reportData);
+        } else if (this.view === 'Tabular view') {
+            this.reportHTML += composeTableHTML(this.reportData);
+        }
+        console.log(this.reportHTML);
+    }
+
+    composePDF() {}
 }
